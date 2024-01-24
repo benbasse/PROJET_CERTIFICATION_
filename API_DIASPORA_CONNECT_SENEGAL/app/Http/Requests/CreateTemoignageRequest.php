ff2254eb547c999aad616b2105972726a159c929
benbasse <moussabasse221@gmail.com>
2024-01-24 16:08:18 +0000

    Validation des donnees pour les temoignage

---
+++ API_DIASPORA_CONNECT_SENEGAL/app/Http/Requests/CreateTemoignageRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CreateTemoignageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "contenue"=> "required|string",
+        ];
+    }
+
+    public function failedValidation(Validator $validator ){
+        throw new HttpResponseException(response()->json([
+            'success'=>false,
+            'status_code'=>422,
+            'error'=>true,
+            'message'=>'erreur de validation',
+            'errorList'=>$validator->errors()
+        ]));
+    }
+
+    public function messages()
+    {
+        return [
+            'contenue.required'=> "le contenue est requis",
+            'contenue.string'=> "le contenue doit contenir que des lettres",
+        ];
+    }
+}
