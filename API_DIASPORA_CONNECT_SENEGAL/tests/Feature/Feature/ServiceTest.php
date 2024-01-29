00860860a522f7c42de696bd297f68e26b97b134
benbasse <moussabasse221@gmail.com>
2024-01-29 11:20:48 +0000

    les testes pour les services

--- API_DIASPORA_CONNECT_SENEGAL/tests/Feature/Feature/ServiceTest.php
+++ API_DIASPORA_CONNECT_SENEGAL/tests/Feature/Feature/ServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,28 +15,44 @@ class ServiceTest extends TestCase
 
     public function test_create_service(): void
     {
+        $user = User::factory()->create();
+        $response = $this->post('/api/auth/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
         $response = $this->post('api/service/create', [
-            'titre'=> 'title 1',
-            'description'=> 'this is the description',
-            'image'=> 'image.png',
+            'titre' => 'title 1',
+            'description' => 'this is the description',
+            'image' => 'image.png',
         ]);
         $response->assertStatus(200);
     }
 
     public function test_delete_service(): void
     {
+        $user = User::factory()->create();
+        $response = $this->post('/api/auth/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
         $response = $this->delete('api/service/supprimer/1');
         $response->assertStatus(200);
     }
     
     public function test_show_service_with_id(): void
     {
+        $user = User::factory()->create();
+        $response = $this->post('/api/auth/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
         $response = $this->get('api/service/detail/1');
         $response->assertStatus(200);
     }
 
     public function test_show_service_with_name(): void
     {
+        
         $response = $this->get('api/service/detail/1');
         $response->assertStatus(200);
     }
