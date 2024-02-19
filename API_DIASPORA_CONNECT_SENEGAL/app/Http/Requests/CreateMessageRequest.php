6cd94a8861c0acb3a412954f56babac4416ff6f9
benbasse <moussabasse221@gmail.com>
2024-02-19 10:58:17 +0000

    Les validation des requetes pour les messages

---
+++ API_DIASPORA_CONNECT_SENEGAL/app/Http/Requests/CreateMessageRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CreateMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'message'=> 'required|string',
+            'email'=> 'required|email',
+        ];
+    }
+
+    public function failedValidation(Validator $validator ){
+        throw new HttpResponseException(response()->json([
+            'success'=>false,
+            'status_code'=>422,
+            'error'=>true,
+            'message'=>'erreur de validation',
+            'errorList'=>$validator->errors()
+        ]));
+    }
+
+    public function messages()
+    {
+        return  [
+            'messages.required'=> 'le message ne peut pas être null',
+            'email.required'=> "l'email est requis",
+            'email.email'=> "email invalide"
+        ];
+    }
+
+}
