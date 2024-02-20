f244bd485d06a9786b524540a8393bc0c31f8751
benbasse <moussabasse221@gmail.com>
2024-02-20 09:45:12 +0000

    mail pour les articles

--- API_DIASPORA_CONNECT_SENEGAL/config/mail.php
+++ API_DIASPORA_CONNECT_SENEGAL/config/mail.php
@@ -37,13 +37,14 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAIL_USERNAME', 'papehamadyfall@gmail.com'),
+            'password' => env('MAIL_PASSWORD', 'nfecrzwbvewskkcb'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'verify_peer' => false,
         ],
 
         'ses' => [
