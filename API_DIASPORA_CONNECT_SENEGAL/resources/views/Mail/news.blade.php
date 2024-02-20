f627bcca1e3e5b50fb95cd28400bf647cdcdc392
benbasse <moussabasse221@gmail.com>
2024-02-20 09:45:42 +0000

    Pour les mails

---
+++ API_DIASPORA_CONNECT_SENEGAL/resources/views/Mail/news.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diaspora Connect</title>
+</head>
+<body>
+This is the view that we will have the articles
+{{$article}}
+
+</body>
+</html>
