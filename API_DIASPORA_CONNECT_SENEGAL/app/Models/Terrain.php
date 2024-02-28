96c6b9f93255832993296e4d1ecfb3494de17734
benbasse <moussabasse221@gmail.com>
2024-02-28 11:51:52 +0000

    Nouvelles fonctionnalites terrains

--- API_DIASPORA_CONNECT_SENEGAL/app/Models/Terrain.php
+++ API_DIASPORA_CONNECT_SENEGAL/app/Models/Terrain.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Terrain extends Model
 {
     use HasFactory;
-
+    protected $fillable = [
+        'est_acheter',
+        
+    ];
     public function User()
     {
         return $this->hasMany(User::class);
